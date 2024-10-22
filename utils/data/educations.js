784e464ac4c7197ed3a5111b334f0bf969af1119
alexcoding77 <alexkcoding@gmail.com>
2024-10-22 15:31:14 -0400

    first commit

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor of Science in Computer Science",
+    duration: "2014 - 2018",
+    institution: "Yerevan State University",
+  },
+  {
+    id: 2,
+    title: "High School Diploma",
+    duration: "2010 - 2014",
+    institution: "Anania Shirakatsi Lyceum",
+  },
+  {
+    id: 3,
+    title: "Middle School",
+    duration: "2004 - 2010",
+    institution: "Yerevan Basic School No. 132",
+  }
+];
